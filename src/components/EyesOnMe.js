86c08043e2d94bf085684bfd4832391d96b33f97
Alex Ju <aju217@gmail.com>
2022-10-12 15:54:38 -0400

    commit tree 409dd13d1240238c67a3983f918e2e31b23b7853

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function handleFocus(e){
+    e.preventDefault();
+    console.log('Good!')
+}
+
+function handleBlur(e){
+    e.preventDefault;
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe(){
+    return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+        Eyes on me
+    </button>
+    )
+}
+
+export default EyesOnMe
